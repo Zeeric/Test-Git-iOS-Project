f984b441ce6a2d021a8b601c6eeb4107a2e8c019
Eric Zeinstra <mmzeinstra@Erics-MacBook-Pro.local>
2014-07-08 15:40:34 -0400

    Initial Commit

---
+++ Git Test/ZEEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZEEAppDelegate.h
+//  Git Test
+//
+//  Created by Eric Zeinstra on 7/8/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZEEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test/ZEEViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZEEViewController.h
+//  Git Test
+//
+//  Created by Eric Zeinstra on 7/8/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZEEViewController : UIViewController
+
+@end
