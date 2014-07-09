5d7f7fd045961d75326f5750b84b1efd1ec9748a
Eric Zeinstra <mmzeinstra@Erics-MacBook-Pro.local>
2014-07-09 15:19:52 -0400

    added a test class

---
+++ ZEETest.h
@@ -0,0 +1,13 @@
+//
+//  ZEETest.h
+//  Git Test
+//
+//  Created by Eric Zeinstra on 7/9/14.
+//  Copyright (c) 2014 Eric Zeinstra. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZEETest : NSObject
+
+@end
---
+++ ZEETest.m
@@ -0,0 +1,13 @@
+//
+//  ZEETest.m
+//  Git Test
+//
+//  Created by Eric Zeinstra on 7/9/14.
+//  Copyright (c) 2014 Eric Zeinstra. All rights reserved.
+//
+
+#import "ZEETest.h"
+
+@implementation ZEETest
+
+@end
